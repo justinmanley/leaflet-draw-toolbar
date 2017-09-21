e7161a3535cd31b56c9efcf0066d167d6bd5ca5f
Justin Manley <manleyjster@gmail.com>
2017-09-20 19:16:22 -0700

    Fix toolbarIcon in DrawActionSpec.

--- test/src/DrawActionSpec.js
+++ test/src/DrawActionSpec.js
@@ -5,7 +5,10 @@ describe("L.Toolbar2.DrawAction", function() {
             var map = { _panes: {} },
                 layer = {};
 
-            var Action = L.Toolbar2.DrawAction.fromHandler(L.Draw.Polyline, "test-class", new L.Toolbar2()),
+            var Action = L.Toolbar2.DrawAction.fromHandler(L.Draw.Polyline, { 
+                    className: 'leaflet-draw-draw-polyline',
+                    tooltip: 'Draw a polyline'
+                }, new L.Toolbar2()),
                 action = new Action(map, layer);
 
             expect(action).to.be.an.instanceof(L.ToolbarAction);
